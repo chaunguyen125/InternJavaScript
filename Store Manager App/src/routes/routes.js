d9451b400792ab5fc85a60bd954bdc20fad289cc
quynhchauhocgioi <betasly125@gmail.com>
2022-11-29 10:24:56 +0700

    define route

--- Store Manager App/src/routes/routes.js
+++ Store Manager App/src/routes/routes.js
@@ -9,50 +9,50 @@ const TestController = require('../app/controllers/test.controller');
 
 function route(app) {
     // Product
-    app.post('/addProducts/', ProductController.createProduct); //Create
-    app.get('/products/', ProductController.findProductsAdv); //Find
+    app.post('/addProducts', ProductController.createProduct); //Create
+    app.get('/products', ProductController.findProductsAdv); //Find
     app.put('/updateProduct/:product_name', ProductController.updateProduct); //Update
-    app.post('/deleteProduct/', ProductController.deleteProduct); //Delete
+    app.post('/deleteProduct', ProductController.deleteProduct); //Delete
 
-    app.post('/filterProduct/', ProductController.readProductByHieusAct); //Read for Hieu bro
-    app.get('/readProductByCategory/', ProductController.readProductByCategory); //Read for Hieu bro
+    app.post('/filterProduct', ProductController.readProductByHieusAct); //Read for Hieu bro
+    app.get('/readProductByCategory', ProductController.readProductByCategory); //Read for Hieu bro
     
     //Store
-    app.get('/findStores/', StoreController.findStores); //Find
-    app.post('/addStores/', StoreController.createStore); //Create
+    app.get('/findStores', StoreController.findStores); //Find
+    app.post('/addStores', StoreController.createStore); //Create
     app.put('/updateStores/:id', StoreController.updateStore); //Update
-    app.post('/deleteStores/', StoreController.deleteStore); //Delete
-    app.get('/readStores/', StoreController.readStore); //Read
+    app.post('/deleteStores', StoreController.deleteStore); //Delete
+    app.get('/readStores', StoreController.readStore); //Read
 
     //Order
-    app.get('/orders/', StoreController.findStores); //Find
-    app.post('/addOrder/', OrderController.createOrder); //Create
+    app.get('/orders', StoreController.findStores); //Find
+    app.post('/addOrder', OrderController.createOrder); //Create
     app.put('/updateOrder/:id', OrderController.updateOrder); //Update
-    app.post('/deleteOrder/', OrderController.deleteOrder); //Delete
-    app.get('/readOrder/', OrderController.readOrder); //Read
+    app.post('/deleteOrder', OrderController.deleteOrder); //Delete
+    app.get('/readOrder', OrderController.readOrder); //Read
 
 
     //Charge
-    app.get('/findCharges/', ChargeController.findCharge); //Find
-    app.post('/addCharge/', ChargeController.createCharge); //Create
+    app.get('/findCharges', ChargeController.findCharge); //Find
+    app.post('/addCharge', ChargeController.createCharge); //Create
     app.put('/updateCharge/:id', ChargeController.updateCharge); //Update
-    app.post('/deleteCharge/', ChargeController.deleteCharge); //Delete
+    app.post('/deleteCharge', ChargeController.deleteCharge); //Delete
 
     //Category
-    app.get('/findCategory/', CategoryController.findCategory); //Find
-    app.post('/addCategory/', CategoryController.createCategory); //Create
+    app.get('/findCategory', CategoryController.findCategory); //Find
+    app.post('/addCategory', CategoryController.createCategory); //Create
     app.put('/updateCategory/:id', CategoryController.updateCategory); //Update
-    app.post('/deleteCategory/', CategoryController.deleteCategory); //Delete
+    app.post('/deleteCategory', CategoryController.deleteCategory); //Delete
 
     //Booking Information
-    app.get('/findBooking/', BookingInformationController.findBookingInformation); //Find
-    app.post('/addBooking/', BookingInformationController.createBookingInformation); //Create
+    app.get('/findBooking', BookingInformationController.findBookingInformation); //Find
+    app.post('/addBooking', BookingInformationController.createBookingInformation); //Create
     app.put('/updateBooking/:id', BookingInformationController.updateBookingInformation); //Update
-    app.post('/deleteBooking/', BookingInformationController.deleteBookingInformation); //Delete
+    app.post('/deleteBooking', BookingInformationController.deleteBookingInformation); //Delete
 
 
     //Test
-    app.get('/test/', TestController.test);
+    app.get('/test', TestController.test);
 
 }
 
