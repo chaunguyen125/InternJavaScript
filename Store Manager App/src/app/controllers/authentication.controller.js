ff497a265b1af67c0844e0fad40d3e577624974a
quynhchauhocgioi <betasly125@gmail.com>
2022-11-30 15:18:35 +0700

    hashing password by using bscrypt lib

--- Store Manager App/src/app/controllers/authentication.controller.js
+++ Store Manager App/src/app/controllers/authentication.controller.js
@@ -2,6 +2,8 @@ const { log } = require('console');
 const dotenv = require("dotenv")
 const db = require('../../config/postgres.config');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 class AuthenticationController {
     async signUp(req, res) {
         // {user_name: value, password: value, address: value, date_of_birth: value, code: value}
@@ -11,26 +13,42 @@ class AuthenticationController {
         let checkUser = await db.query(`select user_name from users where user_name = '${formData["user_name"]}'`);
         console.log("checkuser: " + checkUser);
         if (checkUser.length == 1) return res.json('fail');
-        else db.query(`Insert into users (user_name, password, address, date_of_birth, code) 
-        values ($1, $2, $3, $4, $5) RETURNING *`, Object.values(formData));
-        res.json('success');
+        else {
+            bcrypt.hash(formData["password"], saltRounds, function(err, hash) {
+                // Store hash (pass) in your password DB.
+                formData["password"] = hash;
+                db.query(`Insert into users (user_name, password, address, date_of_birth, code) 
+                values ($1, $2, $3, $4, $5) RETURNING *`, Object.values(formData));
+                res.json('success');
+            });
+            
+        } 
     }
 
     async logIn(req, res) {
         let formData = req.body; //{user_name: value, password=value}
-        console.log("formData: " + formData);
-        let checkUser = await db.query(`select user_name from users where user_name = '${formData["user_name"]}'`);
+        console.log("formData: ");
+        console.log(formData);
+        let checkUser = await db.query(`select user_name, password from users where user_name = '${formData["user_name"]}'`);
+        console.log(checkUser);
         //checkUser if exist
-        if (checkUser.length == 1) {
+        if (checkUser && checkUser.length == 1) {
             console.log("user exist: " + checkUser[0]);
-            let checkPass = await db.query(`select password from users where user_name = '${formData["user_name"]}'`);
+            let checkPass = checkUser[0]["password"];
+            console.log("checkpass");
             console.log(checkPass);
-            if (checkPass[0]["password"] === formData["password"]) {
-                console.log("login");
-                const token = jwt.sign({ data: {user_name: formData["user_name"]} }, process.env.MY_SECRET_KEY, { expiresIn: '1h' });
-                console.log("token: "+token);
-            }
-            else console.log("fail");
+
+            //check pass
+            bcrypt.compare(formData["password"], checkPass, function(err, result) {
+                if (result) {
+                    console.log("login");
+                    const token = jwt.sign({ data: {user_name: formData["user_name"]} }, process.env.MY_SECRET_KEY, { expiresIn: '1h' });
+                    console.log("token: "+token);
+                }
+                else console.log("fail");
+            });
+
+
         }
         res.json('success');
     }
