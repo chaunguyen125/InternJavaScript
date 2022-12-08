1e825d0915218c93d639ff9e68c1935831e32f8b
quynhchauhocgioi <betasly125@gmail.com>
2022-12-08 15:52:57 +0700

    singleton implement

---
+++ DesignPattern/Singleton/singleton.java
@@ -0,0 +1,13 @@
+public class Singleton {
+    private static Singleton instance;
+    private Singleton () {
+
+    } //custom
+
+    public static Singleton getInstance () {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
